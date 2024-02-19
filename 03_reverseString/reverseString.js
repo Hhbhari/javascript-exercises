de40bad67512cae402a2368e7d51b6e784d8b387
Hhbhari <159613769+Hhbhari@users.noreply.github.com>
2024-02-19 20:49:47 +0530

    add answer to excercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(resultString) {
+    return resultString.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    if( typeof(args[0])!=='number' || typeof(args[1])!=='number' || args[0]<0 || args[1]<0 ){
+        return 'ERROR';
+    }
+    else{
+        let sum=0;
+        for(let i=1;i<=Math.max(...args);i++){
+            sum+=i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
